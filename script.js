2a72f45f3958c24b66a502e26ddce5829304ba13
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2022-06-02 17:29:38 -0400

    got etch a sketch to work

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//initial variables
+const gridSize = 16;
+const squareSize = '25px';
+
+
+//creating grid of divs
 function createGrid(size) {
     const container = document.querySelector('#container');
     for (let i = 0; i < size; i++) {
@@ -6,13 +12,27 @@ function createGrid(size) {
         for (let j = 0; j < size; j++) {
             const cell = document.createElement('div');
             cell.className = 'cell';
+            cell.id = (i*size) + j;
             cell.style.border = 'solid';
-            cell.style.width = '25px';
-            cell.style.height = '25px';
+            cell.style.width = squareSize;
+            cell.style.height = squareSize;
             cell.style.margin = '1px';
             row.appendChild(cell);
         }
         container.appendChild(row);
     }
+
 }
-createGrid(16);
+createGrid(gridSize);
+
+
+//adding event listener
+const cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => {
+    cell.addEventListener('mouseover',hover);
+})
+
+//hover function
+function hover() {
+    this.style.backgroundColor = 'black';
+}
